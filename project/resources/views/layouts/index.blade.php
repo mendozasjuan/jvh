b6e94f34984e9c89d2dee22911c36f8480cb48d1
Juan Mendoza <juanmendoza.dev@gmail.com>
2020-08-12 13:50:54 -0400

    Corrigiendo Iconos Regular FontAwesome

--- project/resources/views/layouts/index.blade.php
+++ project/resources/views/layouts/index.blade.php
@@ -16,9 +16,9 @@
 	<!-- Bootstrap -->
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&display=swap" rel="stylesheet">
-	<link rel="stylesheet"  href="{{ asset('css/bootstrap.min.css') }}">
+	<!--<link rel="stylesheet"  href="{{-- asset('css/bootstrap.min.css') --}}">
 
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">-->
 
 	<!-- Styles -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
@@ -42,10 +42,10 @@
 							<img src="img/Capa_1.svg" alt="">
 						</div>
 						<div class="row redes-sociales" style=";font-weight: 500;font-size: 16px;color: #fff">
-							<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-							<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-							<a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+							<a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+							<a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+							<a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+							<a href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a>
 						</div>
 					</div>
 					<div class="col align-items-center">
@@ -55,24 +55,24 @@
 								<div class="col-3">CONTACTO</div>
 							</div>
 							<div class="row" style="margin-bottom: 15px;font-size: 13px;font-weight: 400;">
-								<i class="fa fa-map-marker fa-2x"  aria-hidden="true"></i>
+								<i class="fa fa-map-marker-alt fa-2x"  aria-hidden="true"></i>
 								<div class="col-3">
 									<span style="font-weight: bold;">Dirección</span> <br><span style="color:#fff">Paso 3030 (1752) Lomas del Mirador Buenos Aires Argentina</span>
 								</div>
-								<i class="fa fa-map-marker fa-2x"  aria-hidden="true"></i>
+								<i class="fas fa-map-marker-alt fa-2x"  aria-hidden="true"></i>
 								<div class="col-3"><span style="font-weight: bold;">Dirección</span> <br><span style="color:#fff">Ruta 35, Km 165 (8204) Bernasconi La Pampa - Argentina</span></div>
-								<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+								<i class="far fa-envelope fa-2x" aria-hidden="true"></i>
 								<div class="col-3"><span style="font-weight: bold;">Comercio Exterior</span> <br><span style="color:#fff">Jescudero@frigorificohv.com.ar</span></div>
 							</div>
 							<div class="row" style="font-size: 13px;font-weight: 400;">
-								<i class="fa fa-phone fa-lg" aria-hidden="true"></i>
+								<i class="fas fa-phone-alt fa-2x" aria-hidden="true"></i>
 								<div class="col-3">
 
 									<span style="font-weight: bold;">Llámenos al</span> <br><span style="color:#fff">+54 (11)) 4699-4424 <br>+54 (11)) 4699-2882</span></div>
-								<i class="fa fa-phone fa-2x" aria-hidden="true"></i>
+								<i class="fas fa-phone-alt fa-2x" aria-hidden="true"></i>
 								<div class="col-3"><span style="font-weight: bold;">Llámenos al</span> <br><span style="color:#fff">+54 (11)) 4699-4424 <br>+54 (11)) 4699-2882</span></div>
 
-								<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+								<i class="far fa-envelope fa-2x" aria-hidden="true"></i>
 								<div class="col-3"><span style="font-weight: bold;">Área Comercial</span> <br><span style="color:#fff">dvigna@frigorificohv.com.ar</span></div>
 							</div>
 					</div>
@@ -85,14 +85,11 @@
 
 
 
-	<!-- Jquery -->
-	<script type="text/javascript" src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
-	<!-- Popper -->
-	<script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
-	<!-- Bootstrap -->
-	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+	
 	<!-- Scripts -->
-	<script src="{{ asset('js/app.js') }}" defer></script>
+
+	<script src="{{ asset('js/app.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
+	
 </body>
 </html>
