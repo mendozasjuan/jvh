9abb9d6007b641c3c2374b046e19d26c90277d47
Juan Mendoza <juanmendoza.dev@gmail.com>
2020-08-17 10:15:07 -0400

    cambios en login

--- project/public/js/app.js
+++ project/public/js/app.js
@@ -83692,8 +83692,8 @@ var imagesRootPath = 'storage/';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/jvh/project/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/jvh/project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\jvhs\project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\jvhs\project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
